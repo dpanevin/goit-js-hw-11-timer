9721785f69dc98cef778ceca61f4b3199c818176
Denis Panevin <d.panevin@gmail.com>
2021-05-23 15:24:27 +0300

    Обновляет класс таймера

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ const refs = {
 }
 
 class CountdownTimer {
-    constructor({ selector, targetDate, updateMarkup }) {
+    constructor({ selector, targetDate }) {
         this.selector = selector;
         this.targetDate = targetDate;
-        this.updateMarkup = updateMarkup;
         this._isActive = false;
         this._intervalId = null;
     }
@@ -37,35 +36,34 @@ class CountdownTimer {
         clearInterval(this._intervalId)
     }
 
-    getTimeComponents(time) {
-        const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-        return { days, hours, mins, secs };
-    }
-}
-
-
-const timer1 = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Sep 25, 2021'),
-    updateMarkup: writeTime
-});
-
-timer1.start()
-
-function writeTime({ days, hours, mins, secs }) {
-    refs.days.innerHTML = days;
-    refs.hours.innerHTML = hours;
-    refs.mins.innerHTML = mins;
-    refs.secs.innerHTML = secs;
-}
-
-function pad(value) {
+    pad(value) {
     if (length.value > 2) {
         return value;
     }
     return String(value).padStart(2, '0');
 }
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return { days, hours, mins, secs };
+    }
+
+    updateMarkup({ days, hours, mins, secs }) {
+    refs.days.innerHTML = days;
+    refs.hours.innerHTML = hours;
+    refs.mins.innerHTML = mins;
+    refs.secs.innerHTML = secs;
+}
+}
+
+
+const timer1 = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Sep 17, 2021')
+});
+
+timer1.start()
